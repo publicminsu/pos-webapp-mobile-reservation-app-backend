958d36bb6dad5ae2ddca0a9db66a571801b09ab4
publicminsu <publicminsu@naver.com>
2023-07-03 01:28:40 +0900

    존재하지 않은 리뷰 예외 처리

--- Web/src/main/kotlin/com/hknusc/web/exception/ErrorCode.kt
+++ Web/src/main/kotlin/com/hknusc/web/exception/ErrorCode.kt
@@ -9,6 +9,7 @@ enum class ErrorCode(val status: HttpStatus,val message: String) {
     
     LOGIN_FAIL(HttpStatus.UNAUTHORIZED, "로그인에 실패했습니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED,"유효하지 않은 인증입니다."),
-    EMPTY_TOKEN(HttpStatus.UNAUTHORIZED,"인증되지 않았습니다.")
+    EMPTY_TOKEN(HttpStatus.UNAUTHORIZED,"인증되지 않았습니다."),
 
+    REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND,"리뷰를 찾을 수 없습니다.")
 }
--- Web/src/main/kotlin/com/hknusc/web/service/ReviewService.kt
+++ Web/src/main/kotlin/com/hknusc/web/service/ReviewService.kt
@@ -1,12 +1,19 @@
 package com.hknusc.web.service
 
 import com.hknusc.web.dto.ReviewDTO
+import com.hknusc.web.exception.CustomException
+import com.hknusc.web.exception.ErrorCode
 import com.hknusc.web.repository.ReviewRepository
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class ReviewService(private val reviewRepository: ReviewRepository) {
     fun getReviews(storeId: Int) = reviewRepository.getReviews(storeId)
-    fun getReview(reviewId: Int) = reviewRepository.getReview(reviewId)
+    fun getReview(reviewId: Int): ReviewDTO {
+        try {
+            return reviewRepository.getReview(reviewId)!!
+        } catch (e: Exception) {
+            throw CustomException(ErrorCode.REVIEW_NOT_FOUND)
+        }
+    }
 }
