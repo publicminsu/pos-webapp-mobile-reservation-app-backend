12397d4d27ddb80053060ad084c62c359b2ccbb0
publicminsu <publicminsu@naver.com>
2023-07-30 22:45:31 +0900

    DB에 ENUM을 STRING이 아닌 코드(SMALLINT)로 저장

--- Web/src/main/kotlin/com/hknusc/web/util/type/OrderCode.kt
+++ Web/src/main/kotlin/com/hknusc/web/util/type/OrderCode.kt
@@ -1,10 +1,14 @@
 package com.hknusc.web.util.type
 
-enum class OrderCode(val label: String) {
-    RESERVATION("예약"),
-    RESERVATION_DENY("예약 거부"),
-    RESERVATION_WAIT("예약 승인 대기"),
-    ORDER("주문"),
-    SERVED("서빙 완료"),
-    PAYMENT("결제");
+enum class OrderCode(val label: String, val code: Short) {
+    RESERVATION("예약", 101),
+    RESERVATION_DENY("예약 거부", 102),
+    RESERVATION_WAIT("예약 승인 대기", 103),
+    ORDER("주문", 201),
+    SERVED("서빙 완료", 301),
+    PAYMENT("결제", 401);
+
+    companion object {
+        fun find(code: Short) = OrderCode.values().find { it.code == code }
+    }
 }
