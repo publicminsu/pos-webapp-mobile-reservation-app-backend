00b87aaae5be8885f08bf47b0a944b21fc63b672
publicminsu <publicminsu@naver.com>
2023-09-27 03:24:56 +0900

    예약 취소 상태 코드 추가

--- Web/src/main/kotlin/com/hknusc/web/util/type/OrderCode.kt
+++ Web/src/main/kotlin/com/hknusc/web/util/type/OrderCode.kt
@@ -4,6 +4,7 @@ enum class OrderCode(val label: String, val code: Short) {
     RESERVATION("예약 확정", 101),
     RESERVATION_DENY("예약 거부", 102),
     RESERVATION_WAIT("예약 승인 대기", 103),
+    RESERVATION_CANCEL("예약 취소", 104),
     ORDER("주문", 201),
     SERVED("서빙 완료", 301),
     PAYMENT("결제", 401);
